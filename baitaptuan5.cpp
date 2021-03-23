006ed433d9e83ecc72e45b44f74745149b295471
vdthinh20021444 <20021444@vnu.edu.vn>
2021-03-24 01:13:38 +0700

    BT CONTRO

---
+++ baitaptuan5.cpp
@@ -0,0 +1,14 @@
+// bai 1
+int* getPointerToTen()
+{
+    int *a = new int();
+    *a = 10;
+    return a;
+}
+
+// bai 2
+double* getPointerToPi()
+{
+    double *p = new double(3.14159);
+    return p;
+}
